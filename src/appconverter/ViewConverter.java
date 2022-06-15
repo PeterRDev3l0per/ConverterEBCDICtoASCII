5e02ec08ea67788885a6582ebd724c670d7c2c67
PeterRDev3l0per <yizeckb@gmail.com>
2022-06-15 17:16:07 -0500

    Se agrega proyecto ConverterEBCDIC

---
+++ src/appconverter/ViewConverter.java
@@ -0,0 +1,183 @@
+
+package appconverter;
+
+import java.awt.Color;
+import javax.swing.JOptionPane;
+
+public class ViewConverter extends javax.swing.JFrame {
+
+    public ViewConverter() {
+        initComponents();
+        this.setTitle("** CONVERTIDOR EBCDIC/STRING - V1.0.0 **  | BY:PERG");
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jTextField1 = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jButton1 = new javax.swing.JButton();
+        jComboBox1 = new javax.swing.JComboBox();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTextField1.setFont(new java.awt.Font("Courier New", 1, 18)); // NOI18N
+        jTextField1.setForeground(new java.awt.Color(0, 153, 0));
+        jTextField1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Courier New", 1, 18)); // NOI18N
+        jTextArea1.setForeground(new java.awt.Color(0, 0, 204));
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jButton1.setBackground(new java.awt.Color(0, 153, 51));
+        jButton1.setFont(new java.awt.Font("Courier New", 1, 20)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("CONVERTIR");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jComboBox1.setFont(new java.awt.Font("Courier New", 2, 18)); // NOI18N
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Seleccione formato....", "STRING A EBCDIC", "EBCDIC A STRING" }));
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Courier New", 1, 24)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Seleccione el tipo de conversión");
+
+        jLabel2.setFont(new java.awt.Font("Courier New", 1, 24)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("ENTRADA");
+
+        jLabel3.setFont(new java.awt.Font("Courier New", 1, 24)); // NOI18N
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("SALIDA");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTextField1)
+                    .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 471, Short.MAX_VALUE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 471, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addGap(5, 5, 5)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(8, 8, 8)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String palabra = jTextField1.getText();
+        if (palabra.equals("") || palabra == null) {
+            JOptionPane.showMessageDialog(null, "INGRESE DATOS","¡ERROR!",JOptionPane.WARNING_MESSAGE);
+        }else{
+            int op = jComboBox1.getSelectedIndex();
+            if (op!=0) {
+                if (op==1) {
+                    controller_Converter ctrl_cv = new controller_Converter();
+                    jTextArea1.append("EBCDIC : ["+ctrl_cv.stringToEBCDIC(palabra)+"]\n");
+                }else if(op==2){
+                    controller_Converter ctrl_cv = new controller_Converter();
+                    jTextArea1.append("UNICODE: ["+ctrl_cv.EbcdicToSTRING(palabra)+"]\n");
+                }
+            }else{
+                JOptionPane.showMessageDialog(null, "SELECCIONE OPCIÓN","¡ERROR!",JOptionPane.WARNING_MESSAGE);
+            }
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        int op = jComboBox1.getSelectedIndex();
+        if (op>=0 || op <= 2) {
+            limpiar();
+        }
+    }//GEN-LAST:event_jComboBox1ActionPerformed
+
+    public void limpiar(){
+//        jTextArea1.setText("");
+        jTextField1.setText("");
+    }
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ViewConverter.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ViewConverter.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ViewConverter.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ViewConverter.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ViewConverter().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JButton jButton1;
+    public javax.swing.JComboBox jComboBox1;
+    public javax.swing.JLabel jLabel1;
+    public javax.swing.JLabel jLabel2;
+    public javax.swing.JLabel jLabel3;
+    public javax.swing.JScrollPane jScrollPane1;
+    public javax.swing.JTextArea jTextArea1;
+    public javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/appconverter/controller_Converter.java
@@ -0,0 +1,100 @@
+
+package appconverter;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import javax.swing.JOptionPane;
+
+public class controller_Converter {
+    
+    private static final char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
+    private static final int UPPER_NIBBLE_MASK = 0xF0;
+    private static final int LOWER_NIBBLE_MASK = 0x0F;
+    
+    public String stringToEBCDIC(String palabra){
+        try {
+//            System.out.print("CODING ORIGINAL: "+Arrays.toString(palabra.getBytes())+"\n");
+            // encode to cp1047 represented as Hex
+            byte[] EBCDIC_ARRAY = palabra.getBytes("Cp1047");
+            String ebcdic_array="";
+            for (byte F : EBCDIC_ARRAY) {
+                ebcdic_array+= F&0xFF;
+            }
+//            System.out.print("EBCDIC_ARRAY: "+ebcdic_array+"\n");
+            
+            char[] hex = encodeHex(EBCDIC_ARRAY); 
+            
+            //EN HEX
+            String outs="";
+            System.out.println(Arrays.toString(hex));
+            for (char hex_c: hex) {
+                outs += hex_c;
+            }
+            
+            //REGRESAR AL STRING ORIGINAL
+            String respMsqBytes = new String(decodeHex(hex), "Cp1047");
+//            System.out.println("CODING FORMATO HEX: "+respMsqBytes);
+            return outs;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "ALGÚN CARÁCTER INGRESADO NO VÁLIDO PARA CONVERTIR A EBCDIC.","¡ERROR!",JOptionPane.WARNING_MESSAGE);
+            System.out.println(" stringToEBCDIC(); -> EXCEPCION [" +e+"]");
+            return null;
+        }
+        //H-->48
+    }
+    public String EbcdicToSTRING(String palabra){
+        try {
+            if (palabra.matches("^*[0-9A-Fa-f]*")) {
+                //REGRESAR AL STRING ORIGINAL
+                String outs = new String(decodeHex(palabra.toCharArray()), "Cp1047");
+//                System.out.print("HEXA() TRANSLATE: "+outs+"\n");
+//                outs = new String(new String(decodeHex(palabra.toCharArray()),Charset.forName("UTF-8")).getBytes("Cp1047"), "UTF-8");
+//                System.out.print("UNICODE TRANSLATE: "+outs+"\n");
+                return outs;
+            } else {
+                JOptionPane.showMessageDialog(null, "ALGÚN CARÁCTER INGRESADO NO ES EBCDIC.","¡ERROR!",JOptionPane.WARNING_MESSAGE);
+                return null;
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "ALGÚN CARÁCTER INGRESADO NO ES EBCDIC.","¡ERROR!",JOptionPane.WARNING_MESSAGE);
+            System.out.println(" EbcdicToSTRING(); -> EXCEPCION [" +e+"]");
+            return null;
+        }
+        //H-->48
+    }
+    public char[] encodeHex(final byte[] buf) {
+        final int buflen = buf.length;
+        final char[] ch = new char[buflen * 2];
+        for(int i = 0, j = 0; i < buf.length; i++, j += 2) {
+            final byte b = buf[i];
+            final int upper = (b & UPPER_NIBBLE_MASK) >> 4;
+            final int lower = b & LOWER_NIBBLE_MASK;
+            ch[j] = HEX_DIGITS[upper];
+            ch[j + 1] = HEX_DIGITS[lower];
+        }
+        return ch;
+    }
+    public static byte[] decodeHex(final char[] data) {
+        final int len = data.length;
+        if((len & 0x01) != 0) {
+            throw new IllegalArgumentException("Illegal HexaDecimal character");
+        }
+        final byte[] out = new byte[len >> 1];
+        for(int i = 0, j = 0; j < len; i++) {
+            int f = hexToDigit(data[j], j) << 4;
+            j++;
+            f |= hexToDigit(data[j], j);
+            j++;
+            out[i] = (byte) (f & 0xFF);
+        }
+        return out;
+    }
+    private static int hexToDigit(final char ch, final int index) {
+        final int digit = Character.digit(ch, 16);
+        if(digit == -1) {
+            throw new IllegalArgumentException("Illegal HexaDecimal character '" + ch
+                    + "' at index " + index);
+        }
+        return digit;
+    }
+
+}
